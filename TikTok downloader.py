2d00ea9b98660ca370e4c43a3bb15ce7f96f6f58
biggersmilesj <blog.biggersmile@gmail.com>
2019-11-04 05:10:24 -0800

    Add files via upload

--- TikTok downloader.py
+++ TikTok downloader.py
@@ -6,25 +6,16 @@ import wget
 import sys
 import re
 
-cwd = str(os.getcwd())
-
-try:
-    os.mkdir(cwd + "\\Videos")
-except:
-    pass
-else:
-    pass
-
-cwd += "\\Videos\\"
-
 p1 = r"^https://m.tiktok.com/v/([0-9]{19})"
 p2 = r"^https://www.tiktok.com/([@a-zA-Z\_0-9]+)/video/([0-9]{19})$"
+
 
 def print_menu():
     print("\n\nChoose Option :- \n\n")
     print("\n1) - Without Watermarked")
     print("\n2) - With Watermarker")
     print("\n3) - Audio Only")
+
 
 def get_inputs():
     while(1):
@@ -40,15 +31,16 @@ def get_inputs():
     while(1):
         try:
             opt = int(input("\n\nEnter your option :- "))
-            if(opt<=3) and (opt>=0):
-                opt-=1
+            if(opt <= 3) and (opt >= 0):
+                opt -= 1
                 break
         except:
             print("\nInvalid Input!\nTry again!\n\n")
 
-    download_video(link.strip(),opt)
+    download_video(link.strip(), opt)
 
-def download_video(link,opt):
+
+def download_video(link, opt):
     print(link)
     data = {
         'url': link
@@ -68,8 +60,21 @@ def download_video(link,opt):
 
 
 def save_video(link, name):
-    path = cwd + name
-    wget.download(link, path)
+    cwd = str(os.getcwd())
+
+    if os.name != "posix":
+        try:
+            os.mkdir(cwd + "\\Videos")
+        except:
+            pass
+
+        cwd += "\\Videos\\"
+
+        path = cwd + name
+        wget.download(link, path)
+
+    else:
+        wget.download(link, name)
     print(path)
 
 
